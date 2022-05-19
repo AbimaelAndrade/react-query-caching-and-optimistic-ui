69d7ecbe6c9b4e0a96f346525a4376688499c523
Abimael Andrade <abimaelc.andrade@gmail.com>
2022-05-19 18:35:08 -0300

    feat deploy github pages (#3)
    * refactor: replace name past api (#1)
    
    * feat: corrigido retorno da API fake listagem de produtos
    
    * feat: configurado workflow de deploy no github pages
    
    * Criado exemplo de uso da API fake

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,8 @@ import makeServer from "../server";
 import App from "./App";
 import "./index.css";
 
-if (process.env.NODE_ENV === "development") {
-  makeServer();
-}
+// Create the fake server
+makeServer();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
