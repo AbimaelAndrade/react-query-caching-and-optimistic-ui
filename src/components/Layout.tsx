bf40ce83c70b7c9e12ef97c975ceabd6029dc102
abimaelandrade <abimaelc.andrade@gmail.com>
2022-05-23 17:51:06 -0300

    feat: listagem de produtos tradicional

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,9 +1,9 @@
 import { LogoReactQuery } from "../images/LogoReactQuery";
-import { Conditional, Navbar } from ".";
+import { Either, Navbar } from ".";
 
 export const Layout = ({ children }: ComponentProps) => {
   return (
-    <div className="flex flex-col bg-gray-100 min-h-screen">
+    <div className="flex flex-col min-h-screen">
       <Navbar />
       {children}
     </div>
@@ -11,32 +11,36 @@ export const Layout = ({ children }: ComponentProps) => {
 };
 
 Layout.main = ({ children }: ComponentProps) => (
-  <main className="bg-gray-100 grow">{children}</main>
+  <main className="bg-gray-50 grow">{children}</main>
 );
 
 Layout.footer = ({ children }: ComponentProps) => (
   <footer className="footer p-4 bg-neutral text-neutral-content">
-    <Conditional renderIf={!!children}>{children}</Conditional>
-    <Conditional renderIf={!children}>
-      <div className="flex items-center justify-between w-full">
-        <div className="flex items-center">
-          <a
-            href="https://react-query.tanstack.com/"
-            target="_blank"
-            className="cursor-pointer"
-          >
-            <LogoReactQuery className="w-8 h-8" />
-          </a>
-          <p className="normal-case text-md ml-2">
-            <span className="hidden md:inline-flex">
-              Caching e UI otimista com
-            </span>
-            React Query
-          </p>
+    <Either
+      renderIf={!!children}
+      orRender={
+        <div className="flex items-center justify-between w-full">
+          <div className="flex items-center">
+            <a
+              href="https://react-query.tanstack.com/"
+              target="_blank"
+              className="cursor-pointer"
+            >
+              <LogoReactQuery className="w-8 h-8" />
+            </a>
+            <p className="normal-case text-md ml-2">
+              <span className="hidden md:inline-flex">
+                Caching e UI otimista com
+              </span>
+              React Query
+            </p>
+          </div>
+          <p>Tech talk Omega</p>
         </div>
-        <p>Tech talk Omega</p>
-      </div>
-    </Conditional>
+      }
+    >
+      {children}
+    </Either>
   </footer>
 );
 
---
+++ src/images/Ico.tsx
@@ -0,0 +1,126 @@
+import { SVGProps } from "react";
+
+const Discount = ({ className, ...rest }: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 20 20"
+    fill="none"
+    className={`w-5 ${className}`}
+    {...rest}
+  >
+    <path
+      d="M9.16667 2.5L16.6667 10C17.0911 10.4745 17.0911 11.1922 16.6667 11.6667L11.6667 16.6667C11.1922 17.0911 10.4745 17.0911 10 16.6667L2.5 9.16667V5.83333C2.5 3.99238 3.99238 2.5 5.83333 2.5H9.16667"
+      stroke="currentColor"
+      strokeWidth="1.25"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    ></path>
+    <circle
+      cx="7.50004"
+      cy="7.49967"
+      r="1.66667"
+      stroke="currentColor"
+      strokeWidth="1.25"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    ></circle>
+  </svg>
+);
+
+const View = ({ className, ...rest }: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke="currentColor"
+    className={className}
+    {...rest}
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+      d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+    />
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+      d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
+    />
+  </svg>
+);
+
+const Edit = ({ className, ...rest }: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke="currentColor"
+    className={className}
+    {...rest}
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+      d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"
+    />
+  </svg>
+);
+
+const Remove = ({ className, ...rest }: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke="currentColor"
+    className={className}
+    {...rest}
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+      d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+    />
+  </svg>
+);
+
+const Previous = ({ className, ...rest }: SVGProps<SVGSVGElement>) => (
+  <svg
+    className={`h-5 w-5 ml-2 fill-current ${className}`}
+    version="1.1"
+    xmlns="http://www.w3.org/2000/svg"
+    x="0px"
+    y="0px"
+    viewBox="-49 141 512 512"
+  >
+    <path d="M438,372H36.355l72.822-72.822c9.763-9.763,9.763-25.592,0-35.355c-9.763-9.764-25.593-9.762-35.355,0 l-115.5,115.5C-46.366,384.01-49,390.369-49,397s2.634,12.989,7.322,17.678l115.5,115.5c9.763,9.762,25.593,9.763,35.355,0 c9.763-9.763,9.763-25.592,0-35.355L36.355,422H438c13.808,0,25-11.193,25-25S451.808,372,438,372z"></path>
+  </svg>
+);
+
+const Next = ({ className, ...rest }: SVGProps<SVGSVGElement>) => (
+  <svg
+    className={`h-5 w-5 ml-2 fill-current ${className}`}
+    xmlns="http://www.w3.org/2000/svg"
+    x="0px"
+    y="0px"
+    viewBox="-49 141 512 512"
+  >
+    <path
+      d="M-24,422h401.645l-72.822,72.822c-9.763,9.763-9.763,25.592,0,35.355c9.763,9.764,25.593,9.762,35.355,0
+            l115.5-115.5C460.366,409.989,463,403.63,463,397s-2.634-12.989-7.322-17.678l-115.5-115.5c-9.763-9.762-25.593-9.763-35.355,0
+            c-9.763,9.763-9.763,25.592,0,35.355l72.822,72.822H-24c-13.808,0-25,11.193-25,25S-37.808,422-24,422z"
+    />
+  </svg>
+);
+
+export const Icon = {
+  Discount,
+  View,
+  Edit,
+  Remove,
+  Next,
+  Previous,
+};
