9e4e1664aee2fed79fba1ddf3998917d2a56551b
abimaelandrade <abimaelc.andrade@gmail.com>
2022-05-25 00:17:26 -0300

    feat: removed baseURL

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,12 +2,10 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 import ProductsPage from "./pages/ProductsPage";
 
 export const Router = () => {
-  const baseURL = "/react-query-caching-and-optimistic-ui";
-
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path={[`${baseURL}/`, `${baseURL}/products`]}>
+        <Route exact path={["/", "/products"]}>
           <ProductsPage />
         </Route>
       </Switch>
